c0406c0a612259c08ab31ec0edc2fd19f4f7f1d9
leejaeho-17 <woghdl99@naver.com>
2025-01-07 13:11:39 +0900

    1월 7일

--- javawork/NaverJavaProject/src/day0106db/Ex3PersonTable.java
+++ javawork/NaverJavaProject/src/day0106db/Ex3PersonTable.java
@@ -17,12 +17,12 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class Ex3PersonTable extends JFrame {
-	JTextField tfName,tfAge,tfBlood,tfHp;
-	JButton btnAdd,btnDel;
+	JTextField tfName, tfAge, tfBlood, tfHp;
+	JButton btnAdd, btnDel;
 	DefaultTableModel tableModel;
 	JTable table;
 
-	PersonModel personModel=new PersonModel();//person db 데이터 관리할 클래스
+	PersonModel personModel = new PersonModel();// person db 데이터 관리할 클래스
 
 	public Ex3PersonTable() {
 		// TODO Auto-generated constructor stub
@@ -35,12 +35,12 @@ public class Ex3PersonTable extends JFrame {
 	}
 
 	public void initDesign() {
-		JPanel p1=new JPanel();
+		JPanel p1 = new JPanel();
 		p1.add(new JLabel("이름"));
-		tfName=(new JTextField(4));
-		tfAge=(new JTextField(3));
-		tfBlood=(new JTextField(3));
-		tfHp=(new JTextField(10));
+		tfName = (new JTextField(4));
+		tfAge = (new JTextField(3));
+		tfBlood = (new JTextField(3));
+		tfHp = (new JTextField(10));
 		p1.add(tfName);
 		p1.add(new JLabel("나이"));
 		p1.add(tfAge);
@@ -49,55 +49,51 @@ public class Ex3PersonTable extends JFrame {
 		p1.add(new JLabel("휴대폰"));
 		p1.add(tfHp);
 
-		this.add("North",p1);
+		this.add("North", p1);
 
-		String []title={"번호","이름","나이","휴대폰","혈액형"};
-		tableModel=new DefaultTableModel(title,0);
-		table=new JTable(tableModel);
-		this.add("Center",new JScrollPane(table));
+		String[] title = { "번호", "이름", "나이", "휴대폰", "혈액형" };
+		tableModel = new DefaultTableModel(title, 0);
+		table = new JTable(tableModel);
+		this.add("Center", new JScrollPane(table));
 
 		this.rowSelect();
 
-		btnAdd=new JButton("직원추가");
-		btnDel=new JButton("직원삭제");
+		btnAdd = new JButton("직원추가");
+		btnDel = new JButton("직원삭제");
 
-		JPanel p2=new JPanel();
+		JPanel p2 = new JPanel();
 		p2.add(btnAdd);
 		p2.add(btnDel);
-		this.add("South",p2);
+		this.add("South", p2);
 
-		//직원 추가 버튼 이벤트
+		// 직원 추가 버튼 이벤트
 		btnAdd.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String name=tfName.getText();
-				if(name.length()==0)
-				{
+				String name = tfName.getText();
+				if (name.length() == 0) {
 					JOptionPane.showMessageDialog(Ex3PersonTable.this, "이름을 입력하세요");
 					return;
 				}
 
-				String text_age=tfAge.getText();
-				int age=0;
-				if(text_age.length()==0)
-				{
+				String text_age = tfAge.getText();
+				int age = 0;
+				if (text_age.length() == 0) {
 					JOptionPane.showMessageDialog(Ex3PersonTable.this, "나이를 입력하세요");
 					return;
 				} else {
-					age=Integer.parseInt(text_age);
+					age = Integer.parseInt(text_age);
 				}
-				String blood=tfBlood.getText();
-				if(blood.length()==0)
-				{
+				String blood = tfBlood.getText();
+				if (blood.length() == 0) {
 					JOptionPane.showMessageDialog(Ex3PersonTable.this, "혈액형을 입력하세요");
 					return;
 				}
 
-				String hp=tfHp.getText();
-				if(hp.length()==0)
-				{
+				String hp = tfHp.getText();
+				if (hp.length() == 0) {
 					JOptionPane.showMessageDialog(Ex3PersonTable.this, "전화번호를 입력하세요");
 					return;
 				}
@@ -112,43 +108,41 @@ public class Ex3PersonTable extends JFrame {
 				tfBlood.setText("");
 			}
 		});
-		//직원 삭제 버튼 이벤트
+		// 직원 삭제 버튼 이벤트
 		btnDel.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				int row=table.getSelectedRow();
-				
+				int row = table.getSelectedRow();
+
 				System.out.println(row);
-				
-				if(row==-1)
-				{
+
+				if (row == -1) {
 					JOptionPane.showMessageDialog(Ex3PersonTable.this, "삭제할 행을 먼저 선택하세요");
 					return;
 				}
-				int num=Integer.parseInt((String)table.getValueAt(row, 0));
-				
+				int num = Integer.parseInt((String) table.getValueAt(row, 0));
+
 				personModel.deletePerson(num);
-				
+
 				rowSelect();
 			}
 		});
-
 
 	}
 
 	public void rowSelect() {
 		tableModel.setRowCount(0);
-		List<Vector<String>> list=personModel.getAllDatas();
-		for(Vector<String> data:list)
-		{
+		List<Vector<String>> list = personModel.getAllDatas();
+		for (Vector<String> data : list) {
 			tableModel.addRow(data);
 		}
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ex3PersonTable ex3=new Ex3PersonTable();
+		Ex3PersonTable ex3 = new Ex3PersonTable();
 
 	}
 
---
+++ javawork/NaverJavaProject/src/day0107db/Ex1FoodMysqlDb.java
@@ -0,0 +1,219 @@
+package day0107db;
+
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import day1226.student;
+
+/*
+ * create table foodrest 
+(
+	num smallint auto_increment primary key,
+    foodname varchar(30),
+    foodprice int,
+    foodsize varchar(20) default '보통');
+
+create table foodorder
+(
+	idx smallint auto_increment primary key,
+    num smallint,
+    ordername varchar(20),
+    ordercnt smallint,
+    bookingday varchar(30),
+    constraint fk_foodorder_num foreign key(num) references foodrest(num));
+ */
+public class Ex1FoodMysqlDb extends JFrame {
+	FoodModel foodModel = new FoodModel();
+	DefaultTableModel foodRestTableModel;
+	DefaultTableModel foodOrderTableModel;
+	JTable foodResTable;
+	JTable foodOrderTable;
+
+	JButton btnFoodResAdd, btnFoodResDel;
+	JButton btnFoodOrderAdd, btnFoodOrderDel;
+	JTextField tfFoodNum, tfOrderName, tfOrderCnt, tfBookingDay;
+	JTextField tfFoodName, tfFoodPrice, tfFoodSize;
+
+	public Ex1FoodMysqlDb() {
+		// TODO Auto-generated constructor stub
+		super("메뉴 등록 및 예약");
+		this.setBounds(200, 100, 1000, 400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.initDesign();
+		this.setVisible(true);
+	}
+
+	public void initDesign() {
+		JPanel p1 = new JPanel();
+		btnFoodResAdd = new JButton("메뉴등록");
+		btnFoodResDel = new JButton("메뉴삭제");
+		btnFoodOrderAdd = new JButton("예약하기");
+		btnFoodOrderDel = new JButton("예약취소");
+
+		tfFoodName = new JTextField(7);
+		tfFoodPrice = new JTextField(7);
+		tfFoodSize = new JTextField(5);
+
+		p1.add(new JLabel("메뉴명"));
+		p1.add(tfFoodName);
+		p1.add(new JLabel("가격등록"));
+		p1.add(tfFoodPrice);
+		p1.add(new JLabel("사이즈"));
+		p1.add(tfFoodSize);
+
+		p1.add(btnFoodResAdd);
+		p1.add(btnFoodResDel);
+		p1.add(btnFoodOrderAdd);
+		p1.add(btnFoodOrderDel);
+		this.add("North", p1);
+
+		// 왼쪽에 등록된 메뉴 보이게 하기
+		String[] menuTilte = { "번호", "메뉴명", "가격", "사이즈" };
+		foodRestTableModel = new DefaultTableModel(menuTilte, 0);// 제목과 행갯수는 일단 0으로
+		foodResTable = new JTable(foodRestTableModel);
+		this.add("West", new JScrollPane(foodResTable));
+
+		// 생성된 메뉴 테이블에 db 데이터 출력
+		writeFoodMenu();
+
+		// 가운데 예약테이블 보이게 하기
+		String[] orderTable = { "번호", "예약자", "메뉴명", "가격", "사이즈", "인원수", "예약일" };
+		foodOrderTableModel = new DefaultTableModel(orderTable, 0);
+		foodOrderTable = new JTable(foodOrderTableModel);
+		this.add("Center", new JScrollPane(foodOrderTable));
+
+		// 생성된 예약자 테이블에 예약 내용 출력
+		writeFoodOrderJoin();
+
+		// 하단에 예약내용 입력부분 추가
+		tfOrderName = new JTextField(5);
+		tfOrderCnt = new JTextField(3);
+		tfBookingDay = new JTextField(10);
+		tfFoodNum = new JTextField(3);
+
+		JPanel p2 = new JPanel();
+		p2.add(new JLabel("메뉴번호"));
+		p2.add(tfFoodNum);
+
+		p2.add(new JLabel("예약자명"));
+		p2.add(tfOrderName);
+
+		p2.add(new JLabel("인원수"));
+		p2.add(tfOrderCnt);
+
+		p2.add(new JLabel("예약시간"));
+		p2.add(tfBookingDay);
+
+		this.add("South", p2);
+
+		// 버튼 이벤트들
+
+		// 메뉴 등록 버튼
+		btnFoodResAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String foodName = tfFoodName.getText();
+				int foodprice = Integer.parseInt(tfFoodPrice.getText());
+				String foodSize = tfFoodSize.getText();
+
+				// insert 메서드 호출
+				foodModel.foodMenuInsert(foodName, foodprice, foodSize);
+				// 테이블 다시 출력
+				writeFoodMenu();
+				// 입력 데이터초기화
+				tfFoodName.setText("");
+				tfFoodPrice.setText("");
+				tfFoodSize.setText("");
+			}
+		});
+		// 메뉴 삭제 버튼
+		btnFoodResDel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		// 예약 버튼
+		btnFoodOrderAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				// 입력값들 읽어오기
+				int num = Integer.parseInt(tfFoodNum.getText());
+				String orderName = tfOrderName.getText();
+				int orderCnt = Integer.parseInt(tfOrderCnt.getText());
+				String bookingDay = tfBookingDay.getText();
+
+				// dto 에 넣어주기
+				FoodOrderDto dto = new FoodOrderDto(num, orderName, orderCnt, bookingDay);
+
+				// 예약 insert 메서드 호출하기
+				foodModel.foodOrderInsert(dto);
+
+				// 다시 출력하기
+				writeFoodOrderJoin();
+
+				// 입력값 초기화
+				tfFoodNum.setText("");
+				tfOrderName.setText("");
+				tfOrderCnt.setText("");
+				tfBookingDay.setText("");
+			}
+		});
+		// 예약 취소 버튼
+		btnFoodOrderDel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+
+	public void writeFoodMenu() {
+		// db 에서 전체 등록된 메뉴를 얻는다
+		List<Vector<String>> list = foodModel.getAllMenus();
+		// 기존 메뉴들 삭제
+		foodRestTableModel.setRowCount(0);
+		// table 에 출력
+		for (Vector<String> data : list) {
+			foodRestTableModel.addRow(data);
+		}
+	}
+
+	public void writeFoodOrderJoin() {
+		// order 데이터 가져오기
+		List<Vector<String>> list = foodModel.getAllOrders();
+		// 기존 데이터 모두 삭제
+		foodOrderTableModel.setRowCount(0);
+		// table 에 출력
+		for (Vector<String> data : list) {
+			foodOrderTableModel.addRow(data);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex1FoodMysqlDb ex1 = new Ex1FoodMysqlDb();
+
+	}
+
+}
