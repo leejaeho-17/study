ff791d5da92216cf88d3a74150feed32a831569f
leejaeho-17 <woghdl99@naver.com>
2025-01-13 19:20:12 +0900

    1월 13일

--- javawork/NaverJavaProject/src/day1218/Ex2Lotto.java
+++ javawork/NaverJavaProject/src/day1218/Ex2Lotto.java
@@ -7,59 +7,51 @@ public class Ex2Lotto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int money;
-		int []lotto=new int[6];
-		Random r=new Random();
-		
-		//금액 입력
+		int[] lotto = new int[6];
+		Random r = new Random();
+
+		// 금액 입력
 		System.out.println("금액을 입력해주세요");
-		money=sc.nextInt();
-		
-		//1000원 미만이면 종료
-		if(money<1000)
-		{
+		money = sc.nextInt();
+
+		// 1000원 미만이면 종료
+		if (money < 1000) {
 			System.out.println("** 금액이 부족합니다 **");
 			return;
 		}
-		//금액만큼 로또 숫자 발생해서 출력(오름차순)
-		for(int n=1;n<=money/1000;n++)
-		{
-			System.out.printf("%3d회 : ",n);
-			
-			//lotto 배열에 1~45 사이의 난수발생(중복체크)
-			for(int i=0;i<lotto.length;i++)
-			{
-				lotto[i]=r.nextInt(45)+1;
-				
-				for(int j=0;j<i;j++)
-				{
-					if(lotto[i]==lotto[j])
-					{
+		// 금액만큼 로또 숫자 발생해서 출력(오름차순)
+		for (int n = 1; n <= money / 1000; n++) {
+			System.out.printf("%3d회 : ", n);
+
+			// lotto 배열에 1~45 사이의 난수발생(중복체크)
+			for (int i = 0; i < lotto.length; i++) {
+				lotto[i] = r.nextInt(45) + 1;
+
+				for (int j = 0; j < i; j++) {
+					if (lotto[i] == lotto[j]) {
 						i--;
 						break;
 					}
 				}
 			}
-			//오름차순 정렬
-			for(int i=0;i<lotto.length;i++)
-			{
-				for(int j=i+1;j<lotto.length;j++)
-				{
-					if(lotto[i]>lotto[j])//오름차순
-					//if(lotto[i]<lotto[j])//내림차순
+			// 오름차순 정렬
+			for (int i = 0; i < lotto.length; i++) {
+				for (int j = i + 1; j < lotto.length; j++) {
+					if (lotto[i] > lotto[j])// 오름차순
+					// if(lotto[i]<lotto[j])//내림차순
 					{
-						int temp=lotto[i];
-						lotto[i]=lotto[j];
-						lotto[j]=temp;
+						int temp = lotto[i];
+						lotto[i] = lotto[j];
+						lotto[j] = temp;
 					}
 
 				}
 			}
-			//출력
-			for(int i=0;i<lotto.length;i++)
-			{
-				System.out.printf("%4d",lotto[i]);
+			// 출력
+			for (int i = 0; i < lotto.length; i++) {
+				System.out.printf("%4d", lotto[i]);
 			}
 			System.out.println();
 		}
